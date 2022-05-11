d01ba37de0184e4cbae24e0942d21a8822757b28
최영찬 <gks54410@gmail.com>
2022-05-11 18:25:18 +0900

    Feat: 계좌 이체 기능 추가

---
+++ spring-db1/src/main/java/springdb/springdb1/service/MemberServiceV1.java
@@ -0,0 +1,27 @@
+package springdb.springdb1.service;
+
+import lombok.RequiredArgsConstructor;
+import springdb.springdb1.domain.Member;
+import springdb.springdb1.repository.MemberRepositoryV1;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member member) {
+        if(member.getMemberId().equals("ex")){
+            throw new IllegalStateException("이체 중 예외");
+        }
+    }
+}
