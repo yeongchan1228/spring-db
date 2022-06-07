c6c7e0972fdcbc48bf72de4e80593e69ff541d2f
최영찬 <gks54410@gmail.com>
2022-06-08 01:22:49 +0900

    Feat: Service Interface 추가

---
+++ spring-db2/src/main/java/springdb/springdb2/item/service/ItemService.java
@@ -0,0 +1,19 @@
+package springdb.springdb2.item.service;
+
+import springdb.springdb2.item.entity.Item;
+import springdb.springdb2.item.repository.ItemSearchCond;
+import springdb.springdb2.item.repository.dto.ItemUpdateDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemService {
+
+    Item save(Item item);
+
+    void update(Long itemId, ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findItems(ItemSearchCond itemSearch);
+}
