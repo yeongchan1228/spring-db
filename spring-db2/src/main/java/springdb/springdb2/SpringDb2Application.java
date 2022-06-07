8771024044ce50e588e45475f3a90e8e3781a06a
최영찬 <gks54410@gmail.com>
2022-06-08 01:25:23 +0900

    Feat: MemoryConfig 등록 및 TestDataInit Bean 등록 추가

--- spring-db2/src/main/java/springdb/springdb2/SpringDb2Application.java
+++ spring-db2/src/main/java/springdb/springdb2/SpringDb2Application.java
@@ -2,12 +2,24 @@ package springdb.springdb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import springdb.springdb2.item.config.MemoryConfig;
+import springdb.springdb2.item.repository.ItemRepository;
 
-@SpringBootApplication
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "springdb.springdb2.item.controller")
 public class SpringDb2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDb2Application.class, args);
+	}
+
+	@Bean
+	@Profile("local")
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
 	}
 
 }
