4ca3299dc4a4e89557f4276d2117e558f9485714
최영찬 <gks54410@gmail.com>
2022-06-08 01:24:25 +0900

    Feat: Item 검색 조건 객체 추가

---
+++ spring-db2/src/main/java/springdb/springdb2/item/repository/ItemSearchCond.java
@@ -0,0 +1,18 @@
+package springdb.springdb2.item.repository;
+
+import lombok.Data;
+
+@Data
+public class ItemSearchCond {
+
+    private String itemName;
+    private Integer maxPrice;
+
+    public ItemSearchCond() {
+    }
+
+    public ItemSearchCond(String itemName, Integer maxPrice) {
+        this.itemName = itemName;
+        this.maxPrice = maxPrice;
+    }
+}
