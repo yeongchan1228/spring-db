f16e16125588c9a813d5aa4f5f1e81ed3b02f28e
최영찬 <gks54410@gmail.com>
2022-06-08 01:06:15 +0900

    Initial Commit

---
+++ spring-db2/src/main/java/springdb/springdb2/SpringDb2Application.java
@@ -0,0 +1,13 @@
+package springdb.springdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDb2Application.class, args);
+	}
+
+}
---
+++ spring-db2/src/test/java/springdb/springdb2/SpringDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package springdb.springdb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
