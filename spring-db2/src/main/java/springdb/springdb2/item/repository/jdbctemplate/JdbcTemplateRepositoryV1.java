c8925ee5070cecc07b5403fb7b32e969d332ccf5
최영찬 <gks54410@gmail.com>
2022-06-11 17:59:18 +0900

    Feat: JdbcTemplate 사용 RepositoryV1 구현

---
+++ spring-db2/src/main/java/springdb/springdb2/item/repository/jdbctemplate/JdbcTemplateRepositoryV1.java
@@ -0,0 +1,111 @@
+package springdb.springdb2.item.repository.jdbctemplate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+import springdb.springdb2.item.entity.Item;
+import springdb.springdb2.item.repository.ItemRepository;
+import springdb.springdb2.item.repository.ItemSearchCond;
+import springdb.springdb2.item.repository.dto.ItemUpdateDto;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+public class JdbcTemplateRepositoryV1 implements ItemRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateRepositoryV1(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name, price, quantity) values (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // Database에서 생성한 자동 키 값을 받기 위해 생성
+        jdbcTemplate.update(con -> {
+            // 자동 증가 키
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, item.getItemName());
+            ps.setInt(2, item.getPrice());
+            ps.setInt(3, item.getQuantity());
+            return ps;
+        }, keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item set item_name=?, price=?, quantity=? where id=?";
+
+        jdbcTemplate.update(sql, updateParam.getItemName(), updateParam.getPrice(), updateParam.getQuantity(), itemId);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+
+        String sql = "select id, item_name, price, quantity from item where id=?";
+
+        try{
+            Item item = jdbcTemplate.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item);
+        }catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+        String sql = "select id, item_name, price, quantity from item";
+
+        // 동적 쿼리
+        if(StringUtils.hasText(itemName) || maxPrice != null){
+            sql += " where";
+        }
+
+        boolean andFlag = false;
+        ArrayList<Object> param = new ArrayList<>();
+        if(StringUtils.hasText(itemName)){
+            sql += " item_anme like concat('%', ?, '%')";
+            param.add(itemName);
+            andFlag = true;
+        }
+
+        if(maxPrice != null){
+            if(andFlag){
+                sql += " and price <= ?";
+            }else{
+                sql += " price <= ?";
+            }
+        }
+
+        return jdbcTemplate.query(sql, itemRowMapper(), param.toArray());
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return ((rs, rowNum) -> {
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+
+            return item;
+        });
+    }
+}
