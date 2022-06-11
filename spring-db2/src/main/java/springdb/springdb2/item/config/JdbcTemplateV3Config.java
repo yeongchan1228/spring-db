17cbefa6e7b465f26f080032b02626a9b2140c59
최영찬 <gks54410@gmail.com>
2022-06-11 18:59:41 +0900

    Feat: JdbcTemplateRepositoryV3 Bean 등록

---
+++ spring-db2/src/main/java/springdb/springdb2/item/config/JdbcTemplateV3Config.java
@@ -0,0 +1,28 @@
+package springdb.springdb2.item.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springdb.springdb2.item.repository.ItemRepository;
+import springdb.springdb2.item.repository.jdbctemplate.JdbcTemplateRepositoryV3;
+import springdb.springdb2.item.service.ItemService;
+import springdb.springdb2.item.service.ItemServiceV1;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV3Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService(){
+        return new ItemServiceV1(itemRepository( ));
+    }
+
+    @Bean
+    public ItemRepository itemRepository(){
+        return new JdbcTemplateRepositoryV3(dataSource);
+    }
+}
