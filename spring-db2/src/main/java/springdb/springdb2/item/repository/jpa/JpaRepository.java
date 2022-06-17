ae7bfd258358829937f2436cdf29b4a8e4fe1508
최영찬 <gks54410@gmail.com>
2022-06-17 18:05:22 +0900

    Feat: Jpa 구현

---
+++ spring-db2/src/main/java/springdb/springdb2/item/repository/jpa/JpaRepository.java
@@ -0,0 +1,78 @@
+package springdb.springdb2.item.repository.jpa;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+import springdb.springdb2.item.entity.Item;
+import springdb.springdb2.item.repository.ItemRepository;
+import springdb.springdb2.item.repository.ItemSearchCond;
+import springdb.springdb2.item.repository.dto.ItemUpdateDto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Transactional
+public class JpaRepository implements ItemRepository {
+
+    private final EntityManager em;
+
+    public JpaRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return Optional.ofNullable(em.find(Item.class, id));
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String jpql = "select i from Item i";
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        if(StringUtils.hasText(itemName) || maxPrice != null){
+            jpql += " where";
+        }
+
+        boolean andFlag = false;
+        if(StringUtils.hasText(itemName)){
+            jpql += " i.itemName like concat('%', :itemName, '%')";
+            andFlag = true;
+        }
+
+        if(maxPrice != null){
+            if(andFlag){
+                jpql += " and";
+            }
+             jpql += " i.price <= :maxPrice";
+        }
+
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+        if(StringUtils.hasText(itemName)) query.setParameter("itemName", itemName);
+        if(maxPrice != null) query.setParameter("maxPrice", maxPrice);
+
+        return query.getResultList();
+    }
+}
